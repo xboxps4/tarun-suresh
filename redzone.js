3c0cfcd809918cf026aac07e953f85418372de9e
xboxps4 <67192301+xboxps4@users.noreply.github.com>
2020-07-10 14:56:59 +0530

    Add files via upload

---
+++ redzone.js
@@ -0,0 +1,45 @@
+class Red
+{
+  constructor(x, y, width, height) {
+    var options = {
+            'restitution':0.8,
+            'friction':1,
+            'density':1.0
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        
+        World.add(world, this.body);
+
+    }
+
+    display(){
+        var pos =this.body.position;
+        var angle = this.body.angle;
+        push();
+        translate(pos.x, pos.y);
+        rectMode(CENTER);
+        fill("red");
+        rect(0, 0, this.width, this.height);
+        pop();
+        Matter.Body.setStatic(this.body, true);
+      }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
---
+++ sketch.js
@@ -0,0 +1,72 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var box,box1,box2;
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	engine = Engine.create();
+	world = engine.world;
+	box  = new Red(400,680,200,20);
+	box1 = new Red(200,650,20,100);
+	box2 = new Red(600,850,20,100);
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:1, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  box.display();
+  box1.display();
+  box2.display();
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+    Matter.Body.setStatic(packageBody, false);
+    
+  }
+}
+
+
+
